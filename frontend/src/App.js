0883bbf8c5e9b7a69c8f22cda728b0210066acda
Justin Nge <ping.nge@hotmail.com>
2020-11-22 18:56:35 +1100

    UPDATE: Home, Product and basic components done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to Proshop</h1>
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Container className="py-3">
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
