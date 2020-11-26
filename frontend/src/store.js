4c5699e12306bd0ed8e6d4a69bd29c5384cc0d09
Justin Nge <ping.nge@hotmail.com>
2020-11-26 20:33:06 +1100

    UPDATE: fixed cartItems bug, did not add cartItems in the cart object in store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { cartReducer } from './reducers/cartReducers';
 import {
   productListReducers,
   productDetailsReducers
 } from './reducers/productReducers';
-import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productListReducers,
@@ -17,7 +17,11 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
-const initialState = { cart: cartItemsFromStorage };
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage
+  }
+};
 
 const middleware = [thunk];
 
